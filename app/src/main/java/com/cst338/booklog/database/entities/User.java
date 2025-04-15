1cdb1d01b2a08d7189749291534e1d773325d12b
tom <tvandergroen@csumb.edu>
2025-04-15 13:35:46 -0700

    Created User Entity: + added fields and set 'id' field with PK w/autoGeneration + generated Constructor w/two parameters + generated getter and setter methods for all fields + generated equals and hashcode methods

--- app/src/main/java/com/cst338/booklog/database/entities/User.java
+++ app/src/main/java/com/cst338/booklog/database/entities/User.java
@@ -1,4 +1,65 @@
 package com.cst338.booklog.database.entities;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+import com.cst338.booklog.database.BookLogDatabase;
+import java.util.Objects;
+
+@Entity(tableName = BookLogDatabase.USER_TABLE)
 public class User {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String userName;
+    private String password;
+    private boolean isAdmin;
+
+    public User(String userName, String password){
+        this.password = password;
+        this.userName = userName;
+        isAdmin = false;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && isAdmin == user.isAdmin && Objects.equals(userName, user.userName) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userName, password, isAdmin);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
 }
