cb3ebcdde3f5c29b6db0f784fac9a91b37afbdc6
aleczambie <aleczambie@gmail.com>
2025-04-22 16:30:15 -0700

    created xml files for 2 pages. need change

--- app/src/main/java/com/cst338/booklog/database/BookDAO.java
+++ app/src/main/java/com/cst338/booklog/database/BookDAO.java
@@ -22,9 +22,21 @@ public interface BookDAO {
     @Delete
     void delete(Book book);
 
-    @Query("SELECT * FROM " + BookLogDatabase.BOOK_TABLE)
+    @Query("SELECT * FROM " + BookLogDatabase.BOOK_TABLE + " ORDER by author")
     LiveData<List<Book>> getAllBooks();
 
-    @Query("SELECT * FROM " + BookLogDatabase.BOOK_TABLE + " WHERE genre == :genre")
-    LiveData<List<Book>> getBooksByGenre(String genre);
+//    @Query("SELECT * FROM " + BookLogDatabase.BOOK_TABLE + " WHERE genre == :bookGenre")
+//    LiveData<List<Book>> getBooksByGenre(String bookGenre);
+
+    @Query("SELECT * FROM " + BookLogDatabase.BOOK_TABLE + " WHERE title == :bookTitle")
+    LiveData<List<Book>> getBookByTitle(String bookTitle);
+
+    @Query("SELECT * FROM " + BookLogDatabase.BOOK_TABLE + " WHERE title == :bookId ")
+    LiveData<List<Book>> getBookById(String bookId);
+
+//    @Query("SELECT * FROM " + BookLogDatabase.BOOK_TABLE + " WHERE title == :bookAuthor")
+//    LiveData<List<Book>> getBooksByAuthor(String bookAuthor);
+//
+
+
 }
--- app/src/main/java/com/cst338/booklog/database/entities/Book.java
+++ app/src/main/java/com/cst338/booklog/database/entities/Book.java
@@ -5,6 +5,8 @@ import androidx.room.PrimaryKey;
 
 import com.cst338.booklog.database.BookLogDatabase;
 
+import java.util.Objects;
+
 @Entity(tableName = BookLogDatabase.BOOK_TABLE)
 public class Book {
     @PrimaryKey(autoGenerate = true)
@@ -50,5 +52,19 @@ public class Book {
 
     public void setGenre(String genre) {
         this.genre = genre;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return id == book.id && Objects.equals(title, book.title) && Objects.equals(author, book.author)
+                && Objects.equals(genre, book.genre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, author, genre);
     }
 }
