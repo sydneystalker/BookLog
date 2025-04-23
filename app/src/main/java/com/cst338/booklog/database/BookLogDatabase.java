87df1f5c49b04ecb27c9cf02739871b954a8589b
tom <tvandergroen@csumb.edu>
2025-04-23 14:40:54 -0700

    * Removed anything pertaining to Genre.class: + Genre.java + GenreDAO.Java + BookLogDatabase: - Predefined genres removed from BookLogDatabase. - Genre class removed as entity - Genre import removed from BookLogDatabase.Java file.

--- app/src/main/java/com/cst338/booklog/database/BookLogDatabase.java
+++ app/src/main/java/com/cst338/booklog/database/BookLogDatabase.java
@@ -18,13 +18,12 @@ import com.cst338.booklog.database.entities.BookLog;
 
 import com.cst338.booklog.database.entities.User;
 import com.cst338.booklog.database.typeConverters.LocalDateTypeConverters;
-import com.cst338.booklog.database.entities.Genre;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @TypeConverters(LocalDateTypeConverters.class)
-@Database(entities = {BookLog.class, User.class, Book.class, Genre.class}, version = 1, exportSchema = false)
+@Database(entities = {BookLog.class, User.class, Book.class}, version = 1, exportSchema = false)
 public abstract class BookLogDatabase extends RoomDatabase {
     public static final String USER_TABLE = "userTable";
     public static final String BOOK_TABLE = "bookTable";
@@ -64,14 +63,6 @@ public abstract class BookLogDatabase extends RoomDatabase {
                 dao.insert(admin);
                 User testUser1 = new User("testuser1", "testuser1");
                 dao.insert(testUser1);
-
-                GenreDAO genreDao = INSTANCE.genreDAO();
-                genreDao.deleteAll();
-                genreDao.insert(new Genre("Fiction"));
-                genreDao.insert(new Genre("Non-Fiction"));
-                genreDao.insert(new Genre("Fantasy"));
-                genreDao.insert(new Genre("Romance"));
-                genreDao.insert(new Genre("Science Fiction"));
             });
         }
     };
--- app/src/main/java/com/cst338/booklog/database/GenreDAO.java
+++
@@ -1,7 +0,0 @@
-package com.cst338.booklog.database;
-
-import androidx.room.Dao;
-
-@Dao
-public interface GenreDAO {
-}
